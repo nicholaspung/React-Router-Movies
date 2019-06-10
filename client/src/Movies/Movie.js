de2ad3739c56f5e8b9d39c558db8823efab98337
Nicholas Pung <nicholaspung>
2019-06-10 12:22:28 -0700

    got the movielist routing to work

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
@@ -38,6 +38,7 @@ export default class Movie extends Component {
   // }
 
   render() {
+    console.log(this.state)
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Movie from './Movie';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -24,7 +26,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <Link to={`/movies/${movie.id}`}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link>
         ))}
       </div>
     );
