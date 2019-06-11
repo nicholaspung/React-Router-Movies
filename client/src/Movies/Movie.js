a4731fcaf97ec70369d79f16782934a734b4473b
Nicholas Pung <nicholaspung>
2019-06-11 08:42:55 -0700

    changed the saved list to links and changed them to navlinks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,14 +6,13 @@ export default class Movie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: null,
-      props
+      movie: null
     };
   }
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = this.state.props.match.params.id;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
@@ -27,17 +26,18 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  // Uncomment this code when you're ready for the stretch problems
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
@@ -47,7 +47,7 @@ export default class Movie extends Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie}/>
-        <div className="save-button"onClick={() => this.state.props.addToSavedList(this.state.movie)}>Save</div>
+        <div className="save-button"onClick={this.saveMovie}>Save</div>
       </div>
     );
   }
